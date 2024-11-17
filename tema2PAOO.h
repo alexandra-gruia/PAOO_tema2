19f975e027d7c22dac9fbb5cdb4e897971069159
alexandra-gruia <128381596+alexandra-gruia@users.noreply.github.com>
2024-11-17 17:03:51 +0200

    Update tema2PAOO.h

--- tema2PAOO.h
+++ tema2PAOO.h
@@ -1,6 +1,28 @@
 #ifndef tema2PAOO_H
 #define tema2PAOO_H
 
+#include <string>
+using namespace std;
 
+class tema2PAOO {
+private:
+    int x;
+    string nume;
+
+public:
+    // Constructor
+    tema2PAOO(int x_val, const string& nume_val);
+
+    // Destructor
+    ~tema2PAOO();
+
+    // Constructor de copiere
+    tema2PAOO(const tema2PAOO& other);
+
+    // Operator de atribuire
+    tema2PAOO& operator=(const tema2PAOO& other);
+    
+    void print() const;
+};
 
 #endif 

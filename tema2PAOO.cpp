daf2a552c11cc727e7cc9e0ea140ddac8c23f00a
alexandra-gruia <128381596+alexandra-gruia@users.noreply.github.com>
2024-11-17 17:08:29 +0200

    Update tema2PAOO.cpp

--- tema2PAOO.cpp
+++ tema2PAOO.cpp
@@ -1,3 +1,31 @@
 #include "tema2PAOO.h"
 #include <iostream>
+
+// Constructor
+tema2PAOO::tema2PAOO(int x_val, const std::string& nume_val)
+    : x(x_val), nume(nume_val) {}
+
+tema2PAOO::~tema2PAOO() {
+    cout << "Destructorul a fost chemat pentru " << nume << endl;
+}
+
+// Constructor de copiere
+tema2PAOO::tema2PAOO(const tema2PAOO& other) 
+    : x(other.x), nume(other.nume) {
+    cout << "Copy constructorul a fost chemat pentru " << other.nume << endl;
+}
+
+// Operator de atribuire
+tema2PAOO& tema2PAOO::operator=(const tema2PAOO& other) {
+    if (this != &other) {
+        x = other.x;
+        nume = other.nume;
+    }
+    cout << "Operator de atribuire a fost chemat pentru " << other.nume << endl;
+    return *this;
+}
+
+void tema2PAOO::print() const {
+    cout << "x = " << x << ", nume = " << nume << endl;
+}
 using namespace std;

adf85d8bf2c6a01c1ff3b2f38927ddfdd9b21917
alexandra-gruia <128381596+alexandra-gruia@users.noreply.github.com>
2024-11-17 17:08:53 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,21 @@ using namespace std;
 
 int main()
 {
+    tema2PAOO obiect1(10, "Obiect1");
+    obiect1.print(); 
 
+    tema2PAOO obiect2 = obiect1; 
+    obiect2.print();
+
+    tema2PAOO obiect3(20, "Obiect3");
+    obiect3 = obiect1;  
+    obiect3.print();
+    
+    // Constructorul default este sters si va rezulta in eroare
+     //tema2PAOO obiect4;    
+    
+    // Constructorul de copiere este sters si va rezulta in eroare
+    // tema2PAOO obiect5 = obiect2; 
 
   return 0;
 }
